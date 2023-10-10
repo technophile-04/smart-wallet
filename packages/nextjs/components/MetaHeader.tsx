5bac76fc6bd7af62ae87de6e39dc8cfe7985fd4b
Carlos Sánchez <oceanrdn@gmail.com>
2023-10-10 13:26:02 +0200

    New thumbnail (#7)

--- packages/nextjs/components/MetaHeader.tsx
+++ packages/nextjs/components/MetaHeader.tsx
@@ -16,7 +16,7 @@ const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT
 export const MetaHeader = ({
   title = "Scaffold-ETH 2 App",
   description = "Built with 🏗 Scaffold-ETH 2",
-  image = "thumbnail.jpg",
+  image = "thumbnail.png",
   twitterCard = "summary_large_image",
   children,
 }: MetaHeaderProps) => {
